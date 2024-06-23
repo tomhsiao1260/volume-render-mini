672b7fe558077763f60bc3e01aa3e2e8334cbf1c
Hsiao Yao <tomtogo1260@gmail.com>
2024-06-23 21:52:54 +0800

    complete python part

--- readme.md
+++ readme.md
@@ -10,11 +10,13 @@ Install python packages
 pip install -r requirements.txt
 ```
  
-Generate `.nrrd` from `.tif` files
+Generate `.nrrd` from `.tif` files.
 
 ```bash
-python volume.py <TIFF_FOLDER> <OUTPUT_FOLDER> --chunk_size 128 128 128 --start_coord 0 0 0
+python volume.py <INPUT_FOLDER> <OUTPUT_FOLDER> --start_coord 0 0 0 --chunk_size 128 128 128
 ```
+
+`--start_coord` is the starting position (x, y, z) and `--chunk_size` is volume size (x, y, z). You can also add `--png` flag if you want to use `.png` instead of `.tif`.
 
 Install javascript packages
 
--- volume.py
+++ volume.py
@@ -7,11 +7,15 @@ import nrrd
 import argparse
 import tifffile
 import numpy as np
+from PIL import Image
 from pathlib import Path
 
-def tifs2nrrd(tiffdir, nrrddir, chunk_size, start_coord):
+def imgList(tiffdir, png=False):
+    if png: ext = "*.png"
+    else: ext = "*.tif"
+
     # Note this is a generator, not a list
-    tiffs = tiffdir.glob("*.tif")
+    tiffs = tiffdir.glob(ext)
     rec = re.compile(r'([0-9]+)\.\w+$')
 
     inttiffs = {}
@@ -25,7 +29,7 @@ def tifs2nrrd(tiffdir, nrrddir, chunk_size, start_coord):
         ds = match.group(1)
         itiff = int(ds)
         if itiff in inttiffs:
-            err = "File %s: tiff id %d already used"%(tname,itiff)
+            err = "File %s: tiff id %d already used"%(tname, itiff)
             print(err)
             return err
         inttiffs[itiff] = tiff
@@ -36,6 +40,41 @@ def tifs2nrrd(tiffdir, nrrddir, chunk_size, start_coord):
 
     itiffs = list(inttiffs.keys())
     itiffs.sort()
+
+    return inttiffs, itiffs
+
+def pngs2nrrd(tiffdir, nrrddir, chunk_size, start_coord):
+    intpngs, ipngs = imgList(tiffdir, True)
+
+    minz = ipngs[0]
+    maxz = ipngs[-1]
+
+    png0 = Image.open(intpngs[minz])
+    png0 = np.array(png0)
+    h, w = png0.shape
+
+    x0, y0, z0 = start_coord
+    cx, cy, cz = chunk_size
+
+    stack = np.zeros((cz, cy, cx), dtype=np.uint8)
+
+    for layer in range(z0, z0+cz, 1):
+        index = ipngs[layer]
+        png = Image.open(intpngs[index])
+        png = np.array(png)
+
+        stack[layer, :, :] = png[y0:y0+cy, x0:x0+cx]
+
+    # debug
+    filename = os.path.join(nrrddir, 'volume.png')
+    tifffile.imwrite(filename, stack)
+
+    # nrrd for rendering
+    filename = os.path.join(nrrddir, 'volume.nrrd')
+    nrrd.write(filename, stack)
+
+def tifs2nrrd(tiffdir, nrrddir, chunk_size, start_coord):
+    inttiffs, itiffs = imgList(tiffdir, False)
 
     minz = itiffs[0]
     maxz = itiffs[-1]
@@ -52,10 +91,14 @@ def tifs2nrrd(tiffdir, nrrddir, chunk_size, start_coord):
         index = itiffs[layer]
         tiff = tifffile.imread(inttiffs[index])
 
+        # convert to uint8
+        if tiff.dtype == np.uint16:
+            tiff = (tiff / 256).astype(np.uint8)
+
         stack[layer, :, :] = tiff[y0:y0+cy, x0:x0+cx]
 
     # debug
-    filename = os.path.join(nrrddir, 'volume.tif')
+    filename = os.path.join(nrrddir, 'volume.png')
     tifffile.imwrite(filename, stack)
 
     # nrrd for rendering
@@ -71,17 +114,21 @@ def main():
             "output_nrrd_dir", 
             help="Name of directory that will contain nrrd file")
     parser.add_argument(
+            "--start_coord", 
+            nargs=3,
+            type=int,
+            default=[0, 0, 0],
+            help="Starting coordinate (x, y, z)")
+    parser.add_argument(
             "--chunk_size", 
             nargs=3,
             type=int,
             default=[128, 128, 128],
             help="Size of chunk (x, y, z)")
     parser.add_argument(
-            "--start_coord", 
-            nargs=3,
-            type=int,
-            default=[0, 0, 0],
-            help="Starting coordinate (x, y, z)")
+            "--png", 
+            action="store_true", 
+            help="Use .png instead of .tif")
 
     args = parser.parse_args()
 
@@ -95,19 +142,22 @@ def main():
     nrrddir = Path(args.output_nrrd_dir)
     if not nrrddir.exists(): os.mkdir(nrrddir)
 
-    # validate chunk_size
-    chunk_size = args.chunk_size
-    if any(size <= 0 for size in chunk_size):
-        print("Invalid chunk size", chunk_size)
-        return 1
-
     # validate start_coord 
     start_coord = args.start_coord
     if any(p < 0 for p in start_coord):
         print("Invalid starting coordinate", start_coord)
         return 1
 
-    tifs2nrrd(tiffdir, nrrddir, chunk_size, start_coord)
+    # validate chunk_size
+    chunk_size = args.chunk_size
+    if any(size <= 0 for size in chunk_size):
+        print("Invalid chunk size", chunk_size)
+        return 1
+
+    if args.png:
+        pngs2nrrd(tiffdir, nrrddir, chunk_size, start_coord)
+    else:
+        tifs2nrrd(tiffdir, nrrddir, chunk_size, start_coord)
 
 if __name__ == '__main__':
     sys.exit(main())
