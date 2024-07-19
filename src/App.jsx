d911035d7a29570112702b9aa09f5a78ceaae915
Hsiao Yao <tomtogo1260@gmail.com>
2024-07-19 18:30:07 +0800

    renderer & camera setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import * as THREE from 'three'
 import { useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 
 export default function App()
+{
+    const glSettings = { antialias: true, outputEncoding: THREE.sRGBEncoding }
+    const cameraSettings = { fov: 75, near: 0.01, far: 50, position: [ 0, 0, -1.5 ], up: [ 0, -1, 0 ]}
+
+    return <>
+        <Canvas
+            frameloop="demand"
+            camera={ cameraSettings }
+            gl={ glSettings }
+        >
+            <OrbitControls enableDamping={ false } makeDefault />
+            <Cube />
+        </Canvas>
+    </>
+}
+
+export function Cube()
 {
     useFrame((state, delta) =>
     {
         console.log('tick')
     })
 
-    return <>
-        <OrbitControls enableDamping={ false } makeDefault />
-        <Cube />
-    </>
-}
-
-export function Cube()
-{
     return <mesh>
         <boxGeometry args={[ 1, 1, 1 ]}/>
         <meshNormalMaterial />
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import './style.css'
-import { StrictMode } from 'react'
-import { Canvas } from '@react-three/fiber'
+import * as THREE from 'three'
 import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <StrictMode>
-        <Canvas frameloop="demand">
-            <App />
-        </Canvas>
+        <App />
     </StrictMode>
 )
